5757bf979885946f22b9d27ccbeaf767ccac3315
Andrey1914 <anlikajw@gmail.com>
2023-03-20 20:28:21 +0100

    fix liveLinks

--- src/components/Projects/projectsData.js
+++ src/components/Projects/projectsData.js
@@ -22,7 +22,7 @@ export const projectData = [
     transitionImage: transitionBarbershop,
     title: "Template for barbershop studio",
     codeLink: "https://github.com/Andrey1914/barbershop",
-    liveCode: "andrey1914.github.io/barbershop/",
+    liveLink: "https://barbershop-studio.netlify.app/",
   },
   {
     id: 2,
@@ -30,7 +30,7 @@ export const projectData = [
     transitionImage: transitionFilmotekaTeamProject,
     title: "Movie search website",
     codeLink: "https://github.com/Andrey1914/filmoteka-project",
-    liveCode: "https://indisputable09.github.io/filmoteka-project/",
+    liveLink: "https://indisputable09.github.io/filmoteka-project/",
   },
   {
     id: 3,
@@ -38,7 +38,7 @@ export const projectData = [
     transitionImage: tranitionIceCream,
     title: "Online ice cream sale website template",
     codeLink: "https://github.com/Andrey1914/ice-cream",
-    liveCode: "https://andrey1914.github.io/ice-cream/",
+    liveLink: "https://andrey1914.github.io/ice-cream/",
   },
   {
     id: 4,
@@ -46,7 +46,7 @@ export const projectData = [
     transitionImage: bigPhotoFromImageFinder,
     title: "Image search website",
     codeLink: "https://github.com/Andrey1914/goit-js-hw-11-infinite-scroll",
-    liveCode: "https://andrey1914.github.io/goit-js-hw-11-infinite-scroll/",
+    liveLink: "https://andrey1914.github.io/goit-js-hw-11-infinite-scroll/",
   },
   {
     id: 5,
@@ -54,7 +54,7 @@ export const projectData = [
     transitionImage: transitionSlimMom,
     title: "Website for weight management and diet",
     codeLink: "https://github.com/Andrey1914/slim-moms-frontend",
-    liveCode: "https://andriibeha.github.io/slim-moms-frontend/",
+    liveLink: "https://andriibeha.github.io/slim-moms-frontend/",
   },
   {
     id: 6,
@@ -62,7 +62,7 @@ export const projectData = [
     transitionImage: webStudioPortfolio,
     title: "Web studio website template",
     codeLink: "https://github.com/Andrey1914/goit-markup-hw-08",
-    liveCode: "https://andrey1914.github.io/goit-markup-hw-08/",
+    liveLink: "https://andrey1914.github.io/goit-markup-hw-08/",
   },
   {
     id: 7,
@@ -70,6 +70,6 @@ export const projectData = [
     transitionImage: transitionFilmoteka,
     title: "Movie search website",
     codeLink: "https://github.com/Andrey1914/goit-react-hw-05-movies",
-    liveCode: "https://andrey1914.github.io/goit-react-hw-05-movies/",
+    liveLink: "https://andrey1914.github.io/goit-react-hw-05-movies/",
   },
 ];
